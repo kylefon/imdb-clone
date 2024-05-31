936bf1df5245ecb62df823bc2e8b77a839ad8258
unknown <robnekyle@gmail.com>
2024-06-01 01:58:15 +0800

    Add theme dropdown

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -10,25 +10,20 @@ import searchLogo from "./assets/search.svg";
 import { menuImages } from './menuImages'; 
 import { menuItems } from './menuItems';
 import MenuDropDown from './menuDropdown';
+import ThemeDropDown from './themeDropdown';
 
 export default function NavBar() {
 
   const [isHovered, setIsHovered] = useState(null);
+  const [hoverTheme, setHoverTheme] = useState(false);
 
-  const handleMouseEnter = (menu) => {
-    setIsHovered(menu);
-  }
-
-  const handleMouseLeave = () => {
-    setIsHovered(null);
-  }
   return (
     <>
       <nav className='flex gap-6 text-[#c3c3c3] bg-black justify-center p-4 items-center'>
         <img src={IMDBLogo} />
         <ul className='flex gap-4'>
           {Object.keys(menuItems).map((items) => (
-            <li keys = {items} onMouseEnter={() => handleMouseEnter(items)} onMouseLeave={handleMouseLeave}>
+            <li keys = {items} onMouseEnter={() => setIsHovered(items)} onMouseLeave={() => setIsHovered(null)}>
               {items}
             </li>
           ))}
@@ -44,11 +39,24 @@ export default function NavBar() {
           <li><button><img src={bookmark} />Watchlist</button></li>
           <li><button><img src={userLogo}/>User</button></li>
           <li><button>EN<img src={dropdown} /></button></li>
-          <li><img src={systemColor}/></li>
+          <li
+            onMouseEnter={() => setHoverTheme(true)}
+            onMouseLeave={() => setHoverTheme(false)}
+            className='relative'
+          >
+            <img src={systemColor}/>
+            {hoverTheme && (
+              <div className='absolute left-1/2 -translate-x-1/2 p-4'>
+                <ThemeDropDown />
+              </div>
+            )}
+          </li>
         </ul>
       </nav>
       {isHovered && (
-        <MenuDropDown content={menuItems[isHovered]} image={menuImages[isHovered]}/>
+        <div>
+          <MenuDropDown content={menuItems[isHovered]} image={menuImages[isHovered]}/>
+        </div>
       )}
     </>
   )
---
+++ src/themeDropdown.jsx
@@ -0,0 +1,9 @@
+export default function ThemeDropDown() {
+    return (
+        <div className="bg-black text-[#c3c3c3] rounded-md py-3 px-6 leading-loose">
+            <p>Light</p>
+            <p>Dark</p>
+            <p>System</p>
+        </div>
+    )
+}
