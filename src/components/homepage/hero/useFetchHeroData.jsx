e1b631f5fc754ecdb5a1cbb1e6a04e8927f5fdc8
unknown <robnekyle@gmail.com>
2024-06-20 01:20:12 +0800

    Add custom hook for trending movie and tv show api

---
+++ src/components/homepage/hero/useFetchHeroData.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+export const useFetchHeroData = () => {
+    const apiKey = import.meta.env.VITE_API_KEY
+
+    const [heroData, setHeroData] = useState([]);
+
+    useEffect(() => {
+        const fetchHeroData = async () => {
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${apiKey}`
+                }
+            };
+
+            try {
+                const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US`, options);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch movie videos');
+                }
+                const data = await response.json();
+                setHeroData(data.results);
+                console.log(heroData);
+            } catch (error) {
+                console.error('Error fetching movie videos:', error);
+            } 
+        };
+
+        fetchHeroData();
+    }, []);
+
+    return {heroData}; 
+
+}
